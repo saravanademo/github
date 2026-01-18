9f8b98b6ba608c0d113b23902a55f47d3369a21a
demo <sarav97343@gmail.com>
2026-01-18 10:32:00 +0530

    printing

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
  feauture
 console.log("welcome ton the git course");
 
-console.log("welcome to the course");
+console.log("welcome to the playlist");
  main
+
+ for( let i=0;i<5;i++)
+ {
+    console.log("code io");
+    
+ }
+
